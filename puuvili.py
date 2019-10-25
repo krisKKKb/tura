e99f56610a98f5eb53a573b190b6c7b17adc92e0
krisKKKb <55477863+krisKKKb@users.noreply.github.com>
2019-10-25 10:51:31 +0300

    Add files via upload

---
+++ puuvili.py
@@ -0,0 +1,19 @@
+fruits = ["arbuus", "pirn",  "apelsin"]
+print (fruits[0])
+fruits = ["arbuus", "pirn", "apelsin", "kirss"]
+print (fruits[-1])
+fruits[1] = "laim"
+print(fruits)
+if "õun" in fruits:
+    print("Jah, õun on nimekirjas")
+else:
+    print("Õun ei ole nimekirjas")
+print(len(fruits))
+fruits.remove ("apelsin")
+print(fruits)
+fruits.reverse()
+print(fruits)
+fruits.sort()
+print(fruits)
+
+    
