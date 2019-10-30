63fd9857f05c2daec7821c0d03be44533c7276b7
krisKKKb <55477863+krisKKKb@users.noreply.github.com>
2019-10-30 15:56:47 +0200

    Add files via upload

---
+++ yl22.py
@@ -0,0 +1,18 @@
+bot_list = ["kivi", "paber", "käärid"]
+bot_equals = ["S", "P", "R"]
+user_equals = ["S", "P", "R"]
+for x in bot_list:
+    user_input = str(input("Kivi, Paber Või käärid: "))
+    print(user_input)
+    import random
+    print(random.choice(bot_list))
+if bot_equals([]) = user_equals([]):
+    print(Viik)
+elif bot_equals[] < user_equals[]:
+    print("sa võitsid")
+else:
+    print("sa kaotasid")
+    
+    
+    
+    
