25fc2b6aff7ef6db681abcd2eaa0c805f87f6dc9
krisKKKb <55477863+krisKKKb@users.noreply.github.com>
2019-11-20 12:33:57 +0200

    Add files via upload

---
+++ car.py
@@ -0,0 +1,23 @@
+BMW = {
+    "name_bmw": "bmw",
+    "model_bmw": "385i",
+    "year_bmw": 2009,
+    "color_bmw": "white",
+    }
+audi = {
+    "name_audi": "audi",
+    "model_audi": "A4",
+    "year_audi": 1999,
+    "color_audi": "gray",
+    }
+merzedes = {
+    "name_mer": "merzedes",
+    "model_mer": "s560",
+    "year_mer": 2003,
+    "color_mer": "black",
+    }
+for y in BMW:
+    print(BMW["name_bmw"])
+    print(BMW["model_bmw"])
+    print(BMW["year_bmw"])
+    print(BMW["color_bmw"])
